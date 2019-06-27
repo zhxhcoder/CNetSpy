da43dd0cb05f0a0bcf148e1b39003d66d716676d
zhxh <zhxhcoder@gmail.com>
2019-06-27 17:45:34 +0800

    version 1.3.4-com.creditease.netspy

--- netspylib/src/main/java/com/creditease/netspy/internal/data/NetSpyDbOpenHelper.java
+++ netspylib/src/main/java/com/creditease/netspy/internal/data/NetSpyDbOpenHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 class NetSpyDbOpenHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "cnetspylib.db";
-    private static final int VERSION = 6;
+    private static final int VERSION = 7;
 
     NetSpyDbOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, VERSION);
