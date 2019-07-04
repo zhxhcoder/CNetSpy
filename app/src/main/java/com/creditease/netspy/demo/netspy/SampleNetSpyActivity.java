b29610f07ac3058929d6d1fdc1519e9016fdffd8
zhxh <zhxhcoder@gmail.com>
2019-07-04 17:12:51 +0800

    [Feature] forceSendRequestByMobileData 强制使用流量

--- app/src/main/java/com/creditease/netspy/demo/netspy/SampleNetSpyActivity.java
+++ app/src/main/java/com/creditease/netspy/demo/netspy/SampleNetSpyActivity.java
@@ -1,7 +1,15 @@
 package com.creditease.netspy.demo.netspy;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.NetworkRequest;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,11 +21,17 @@ import com.creditease.netspy.NetSpyHelper;
 import com.creditease.netspy.NetSpyInterceptor;
 import com.creditease.netspy.demo.R;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
+import static android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET;
+import static android.net.NetworkCapabilities.TRANSPORT_CELLULAR;
 
 /**
  * Created by zhxh on 2019/06/24
@@ -31,7 +45,7 @@ public class SampleNetSpyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sample_netspy);
         findViewById(R.id.btn_http).setOnClickListener(view -> {
             for (int i = 0; i < 2; i++) {
-                doHttpActivity();
+                forceSendRequestByMobileData(this);
             }
         });
         findViewById(R.id.launch_netspy_directly).setOnClickListener(view -> NetSpyHelper.launchActivity(this)
@@ -46,7 +60,31 @@ public class SampleNetSpyActivity extends AppCompatActivity {
         findViewById(R.id.btn_db).setOnClickListener(view -> {
             Log.d(TAG, "queryEventList " + DBHelper.getInstance().queryEventList(200).get(0).toString());
         });
+        findViewById(R.id.btn_net_state).setOnClickListener(view -> {
+
+            checkOnlineState();
+
+        });
+
+
     }
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if (msg.what == 1) {
+                boolean isReachable = (boolean) msg.obj;
+                if (isReachable) {
+                    Log.d(TAG, "host 可达 ");
+                } else {
+                    Log.d(TAG, "host 不可达 ");
+                }
+            }
+        }
+    };
 
     private OkHttpClient getClient(Context context) {
         return new OkHttpClient.Builder()
@@ -72,5 +110,55 @@ public class SampleNetSpyActivity extends AppCompatActivity {
         api.getList().enqueue(cb);
         api.postList(new SpyApiService.Data("list")).enqueue(cb);
         api.postArray(new SpyApiService.Data("array")).enqueue(cb);
+    }
+
+
+    @TargetApi(21)
+    private void forceSendRequestByMobileData(Context ctx) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkRequest.Builder builder = new NetworkRequest.Builder();
+        builder.addCapability(NET_CAPABILITY_INTERNET);
+        //强制使用蜂窝数据网络-移动数据
+        builder.addTransportType(TRANSPORT_CELLULAR);
+        NetworkRequest build = builder.build();
+        connectivityManager.requestNetwork(build, new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(Network network) {
+                super.onAvailable(network);
+                try {
+                    doHttpActivity();
+
+                } catch (Exception e) {
+
+                }
+            }
+        });
+    }
+
+
+    public void checkOnlineState() {
+        ConnectivityManager CManager =
+            (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo NInfo = CManager.getActiveNetworkInfo();
+        if (NInfo != null && NInfo.isConnectedOrConnecting()) {
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+
+                        boolean isReachable = InetAddress.getByName("5g.yirendai.com").isReachable(10000);
+
+                        Message message = handler.obtainMessage();
+                        message.what = 1001;
+                        message.obj = isReachable;
+                        handler.sendMessage(message);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/creditease/netspy/demo/support/NetHelper.java
@@ -0,0 +1,23 @@
+package com.creditease.netspy.demo.support;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkRequest;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET;
+import static android.net.NetworkCapabilities.TRANSPORT_CELLULAR;
+
+/**
+ * Created by zhxh on 2019/07/04
+ */
+public class NetHelper {
+
+
+
+
+}
