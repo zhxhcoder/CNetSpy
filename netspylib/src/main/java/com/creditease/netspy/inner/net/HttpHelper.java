256ae953f5896870b1a73a1d245d54f831152ec5
zhxh <zhxhcoder@gmail.com>
2019-07-04 08:47:48 +0800

    [Feature] uploadFile上传文件

--- netspylib/src/main/java/com/creditease/netspy/inner/net/HttpHelper.java
+++ netspylib/src/main/java/com/creditease/netspy/inner/net/HttpHelper.java
@@ -7,7 +7,10 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Headers;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -41,6 +44,56 @@ public class HttpHelper {
                 Log.d("XXXXXXXX", "onResponse: " + response.body().string());
             }
         });
+    }
+
+
+    public static void uploadFile(String url, File file) {
+        OkHttpClient client = new OkHttpClient();
+
+        FormBody paramsBody = new FormBody.Builder()
+            .add("id", "")
+            .add("name", "")
+            .build();
+
+        //二种：文件请求体
+        MediaType type = MediaType.parse("application/octet-stream");//"text/xml;charset=utf-8"
+        RequestBody fileBody = RequestBody.create(type, file);
+
+
+        //三种：混合参数和文件请求
+        RequestBody multipartBody = new MultipartBody.Builder()
+            .setType(MultipartBody.ALTERNATIVE)
+            //一样的效果
+            .addPart(Headers.of(
+                "Content-Disposition",
+                "form-data; name=\"params\"")
+                , paramsBody)
+            .addPart(Headers.of(
+                "Content-Disposition",
+                "form-data; name=\"file\"; filename=\"plans.xml\"")
+                , fileBody)
+            .build();
+
+        Request request = new Request.Builder().url(url)
+            .addHeader("User-Agent", "android")
+            .header("Content-Type", "text/html; charset=utf-8;")
+            .post(multipartBody)//传参数、文件或者混合，改一下就行请求体就行
+            .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    Log.d("XXXXXXXX", "onResponse: " + response.body().string());
+                }
+            }
+        });
+
     }
 
 
