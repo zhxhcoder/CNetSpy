fc4e5a923c91762e0e69c7f5336bd382ed693fd4
zhxh <xiaohuizhu4@creditease.cn>
2020-05-19 18:16:34 +0800

    版本3.1.2

--- netspylib/src/main/java/com/creditease/netspy/inner/apm/BlockHelper.java
+++
@@ -1,8 +0,0 @@
-package com.creditease.netspy.inner.apm;
-
-/**
- * Created by zhxh on 2019/07/18
- * 检测是否卡顿
- */
-public class BlockHelper {
-}
--- netspylib/src/main/java/com/creditease/netspy/inner/lifecycle/CLifeObserver.java
+++
@@ -1,9 +0,0 @@
-package com.creditease.netspy.inner.lifecycle;
-
-import android.arch.lifecycle.LifecycleObserver;
-
-/**
- * Created by zhxh on 2019/07/17
- */
-public class CLifeObserver implements LifecycleObserver {
-}
