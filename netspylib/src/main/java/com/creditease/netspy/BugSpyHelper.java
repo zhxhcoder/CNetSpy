80a1cac5dfd167221db6125c6957db4f380f1806
zhxh <zhxhcoder@gmail.com>
2020-05-23 11:01:13 +0800

    BugSpyHelper

---
+++ netspylib/src/main/java/com/creditease/netspy/BugSpyHelper.java
@@ -0,0 +1,11 @@
+package com.creditease.netspy;
+
+/**
+ * Created by zhxh on 2020/05/23
+ */
+public class BugSpyHelper implements Thread.UncaughtExceptionHandler{
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+
+    }
+}
