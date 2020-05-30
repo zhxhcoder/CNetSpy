5218a9bff31605bd0536354d66a770d03d559950
zhxh <zhxhcoder@gmail.com>
2020-05-30 17:34:14 +0800

    v3.2.0优化-deviceId

--- app/src/main/java/com/creditease/netspy/demo/MainActivity.java
+++ app/src/main/java/com/creditease/netspy/demo/MainActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.creditease.netspy.BugSpyHelper;
 import com.creditease.netspy.demo.netspy.SampleSpyActivity;
-import com.creditease.netspy.inner.ui.netspy.HttpTabActivity;
 
 /**
  * Created by zhxh on 2019/06/20
@@ -20,19 +18,24 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.spyButton).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SampleSpyActivity.class)));
 
-        findViewById(R.id.errorButton).setOnClickListener(v -> {
-            int a = 1 / 0;
-
+        findViewById(R.id.errorButton1).setOnClickListener(v -> {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
+                    //子线程崩溃没事？？？
                     int a = 1 / 0;
                 }
             });
+        });
 
+        findViewById(R.id.errorButton2).setOnClickListener(v -> {
+            String a = "哈哈";
+            int b = Integer.parseInt(a);
+        });
+
+        findViewById(R.id.errorButton3).setOnClickListener(v -> {
             String str = null;
             str.substring(2);
-
         });
     }
 }
