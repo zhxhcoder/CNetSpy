2a240edb2e27f377cf8fdc69e82b57a710b22fb9
zhxh <zhxhcoder@gmail.com>
2020-06-01 10:05:26 +0800

    v3.2.1数据库-错误收集

--- app/src/main/java/com/creditease/netspy/demo/MainActivity.java
+++ app/src/main/java/com/creditease/netspy/demo/MainActivity.java
@@ -3,9 +3,16 @@ package com.creditease.netspy.demo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 
 import com.creditease.netspy.demo.netspy.SampleSpyActivity;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zhxh on 2019/06/20
@@ -63,19 +70,35 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
             spyButton.setText("超时错误");
-
         });
         findViewById(R.id.errorButton5).setOnClickListener(v -> {//anr
-
+            try {
+                Thread.sleep(32000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            spyButton.setText("ANR模拟");
 
         });
 
         findViewById(R.id.errorButton6).setOnClickListener(v -> {//OutOfMemory
 
+            final List<byte[]> container = new ArrayList<>();
+
+            Log.e("cAppSpy", "max memory = " + Runtime.getRuntime().maxMemory());
+            Log.e("cAppSpy", "free memory = " + Runtime.getRuntime().freeMemory());
+            Log.e("cAppSpy", "total memory = " + Runtime.getRuntime().totalMemory());
+            byte[] b = new byte[100 * 1000 * 1000];
+            container.add(b);
 
         });
         findViewById(R.id.errorButton7).setOnClickListener(v -> {//noExists
-
+            try {
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream("aa.txt"));
+                Log.e("cAppSpy", bis.toString());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
 
         });
     }
