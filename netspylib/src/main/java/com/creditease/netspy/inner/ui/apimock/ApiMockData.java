03f6347135711570767d284a833310f9a4ec6c0d
zhxh <zhxhcoder@gmail.com>
2020-06-06 16:05:52 +0800

    v4.0.0 api_mock

--- netspylib/src/main/java/com/creditease/netspy/inner/ui/apimock/ApiMockData.java
+++ netspylib/src/main/java/com/creditease/netspy/inner/ui/apimock/ApiMockData.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
  * 数据表
  */
 public class ApiMockData implements Serializable {
-    private long timeStamp;
+    private long time; //最近更新时间戳
     private int status; //返回数据类型 404 500 200等 1表示有数据 0表示无数据 -1表示错误
     private int from; //1表示 app 2表示web
     private boolean show; //请求时，只返回为true的resp
