bcf88d4a50ea17c4ec7f4a53d70aab918f46eeff
zhxh <zhxhcoder@gmail.com>
2020-06-06 15:07:52 +0800

    api_detail

---
+++ netspylib/src/main/java/com/creditease/netspy/inner/ui/apimock/ApiMockDetailActivity.java
@@ -0,0 +1,18 @@
+package com.creditease.netspy.inner.ui.apimock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.creditease.netspy.R;
+
+/**
+ * Created by zhxh on 2020/06/06
+ */
+public class ApiMockDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_api_mock_detail);
+    }
+}
