d3f5f003733987e92623b167a8165c0f20e32ff0
zhxh <zhxhcoder@gmail.com>
2020-06-07 16:12:02 +0800

    v4.0.0 api_mock-http请求

--- netspylib/src/main/java/com/creditease/netspy/inner/ui/apimock/ApiMockData.java
+++ netspylib/src/main/java/com/creditease/netspy/inner/ui/apimock/ApiMockData.java
@@ -8,10 +8,10 @@ import java.io.Serializable;
  * url:http://10.106.156.200:5000/todos
  */
 public class ApiMockData implements Serializable {
-    private long time; //最近更新时间戳
     private int status; //返回数据类型 404 500 200等 1表示有数据 0表示无数据 -1表示错误
-    private int from; //1表示 app 2表示web
-    private boolean show; //请求时，只返回为true的resp
+    private int source; //1表示 app 2表示web
+    private int show; // 1默认表示返回resp_data 0表示返回resp_empty -1表示返回resp_error
+    private String time; //最近更新时间戳
     private String method;
     private String path;
     private String host;
@@ -19,7 +19,8 @@ public class ApiMockData implements Serializable {
     private String base_url;
     private String url;
     private String params;
-    private String resp_data; //gson传
+    private String request_info;
+    private String resp_data; //gson传 默认1
     private String resp_empty; //gson传
-    private String resp_failure; //gson传
+    private String resp_error; //gson传
 }
