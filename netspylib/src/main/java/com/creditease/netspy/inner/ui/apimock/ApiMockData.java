adf9ff1f4a2324f5e791e4b27243e5dc5541dc1e
zhxh <zhxhcoder@gmail.com>
2020-06-10 09:15:42 +0800

    ApiMockData

--- netspylib/src/main/java/com/creditease/netspy/inner/ui/apimock/ApiMockData.java
+++ netspylib/src/main/java/com/creditease/netspy/inner/ui/apimock/ApiMockData.java
@@ -5,22 +5,20 @@ import java.io.Serializable;
 /**
  * Created by zhxh on 2020/6/6
  * 数据表
- * url:http://10.106.156.200:5000/todos
  */
 public class ApiMockData implements Serializable {
-    private int status; //返回数据类型 404 500 200等 1表示有数据 0表示无数据 -1表示错误
-    private int source; //1表示 app 2表示web
-    private int show; // 1默认表示返回resp_data 0表示返回resp_empty -1表示返回resp_error
-    private String time; //最近更新时间戳
-    private String method;
-    private String path;
-    private String host;
-    private String port;//默认5000
-    private String base_url;
-    private String url;
-    private String params;
-    private String request_info;
-    private String resp_data; //gson传 默认1
-    private String resp_empty; //gson传
-    private String resp_error; //gson传
+    public String show_type; // 1默认表示返回resp_data 0表示返回resp_empty -1表示返回resp_error
+    public String path;
+    public String resp_data; //gson传
+    public String resp_empty; //gson传
+    public String resp_error; //gson传
+
+    public String getRespShow() {
+        if ("-1".equals(show_type)) {
+            return resp_error;
+        } else if ("0".equals(show_type)) {
+            return resp_empty;
+        } else
+            return resp_data;
+    }
 }
