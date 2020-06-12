07b8bcb0732d015d174bf05f9674360c9aab8641
zhxh <zhxhcoder@gmail.com>
2020-06-12 17:27:55 +0800

    path_index

--- netspylib/src/main/java/com/creditease/netspy/ApiMockInterceptor.java
+++ netspylib/src/main/java/com/creditease/netspy/ApiMockInterceptor.java
@@ -1,7 +1,9 @@
 package com.creditease.netspy;
 
 import java.io.IOException;
+import java.util.Set;
 
+import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.Request;
@@ -18,8 +20,32 @@ public final class ApiMockInterceptor implements Interceptor {
             Request request = chain.request();
             return chain.proceed(request);
         }
+
         //还要处理同一个接口 删除不相干参数 或者 统一加上一个特殊的参数
         Request request = chain.request();
+
+        String path_index = "";
+        HttpUrl.Builder urlBuilder = request.url().newBuilder();
+
+        if ("GET".equals(request.method())) { // GET方法
+            HttpUrl httpUrl = urlBuilder.build();
+            Set<String> paramKeys = httpUrl.queryParameterNames();
+            for (String key : paramKeys) {
+                if ("path_index".equals(key)) {
+                    path_index = "___" + httpUrl.queryParameter(key);
+                }
+            }
+        } else {
+            if (request.body() instanceof FormBody) {
+                FormBody formBody = (FormBody) request.body();
+                for (int i = 0; i < formBody.size(); i++) {
+                    if ("path_index".equals(formBody.name(i))) {
+                        path_index = "___" + formBody.value(i);
+                    }
+                }
+            }
+        }
+
         //获取request的创建者builder
         HttpUrl oldHttpUrl = request.url();
         HttpUrl newHttpUrl = oldHttpUrl
@@ -27,7 +53,7 @@ public final class ApiMockInterceptor implements Interceptor {
                 .scheme("http")
                 .host(ApiMockHelper.host)
                 .port(5000)
-                .encodedPath("/" + oldHttpUrl.encodedPath().replace("/", "__").substring(2))
+                .encodedPath("/" + oldHttpUrl.encodedPath().replace("/", "__").substring(2) + path_index)
                 .build();
         Request.Builder builder = request.newBuilder();
         return chain.proceed(builder.url(newHttpUrl).build());
