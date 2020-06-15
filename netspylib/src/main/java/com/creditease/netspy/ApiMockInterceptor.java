d78a475fb9c4a6208776fecba24b488bd8c4bf2f
zhxh <zhxhcoder@gmail.com>
2020-06-15 18:39:22 +0800

    v4.1.4 与interceptor获取参数的方法一致

--- netspylib/src/main/java/com/creditease/netspy/ApiMockInterceptor.java
+++ netspylib/src/main/java/com/creditease/netspy/ApiMockInterceptor.java
@@ -1,18 +1,34 @@
 package com.creditease.netspy;
 
 import android.text.TextUtils;
-import android.util.Log;
 
+import com.creditease.netspy.inner.db.HttpEvent;
 import com.creditease.netspy.inner.support.FormatHelper;
 
+import java.io.EOFException;
 import java.io.IOException;
-import java.util.Set;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-import okhttp3.FormBody;
+import okhttp3.Headers;
 import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okhttp3.internal.http.HttpHeaders;
+import okio.Buffer;
+import okio.BufferedSource;
+import okio.GzipSource;
+import okio.Okio;
 
 /**
  * Created by zhxh on 2020/6/8
@@ -20,6 +36,9 @@ import okhttp3.Response;
  */
 public final class ApiMockInterceptor implements Interceptor {
     public static final String TAG = "ApiMock";
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+
+    private long maxContentLength = 250000L;
 
     @Override
     public Response intercept(Chain chain) throws IOException {
@@ -28,71 +47,113 @@ public final class ApiMockInterceptor implements Interceptor {
             return chain.proceed(request);
         }
 
-        //还要处理同一个接口 删除不相干参数 或者 统一加上一个特殊的参数
+        String strRequest = "";
+
         Request request = chain.request();
+        RequestBody requestBody = request.body();
+        boolean hasRequestBody = requestBody != null;
 
-        HttpUrl.Builder urlBuilder = request.url().newBuilder();
+        HttpEvent transaction = new HttpEvent();
+        transaction.setRequestDate(new Date());
 
-        StringBuilder pathParams = new StringBuilder();
+        transaction.setMethod(request.method());
+        transaction.setUrl(request.url().toString());
 
-        if ("GET".equals(request.method())) { // GET方法
-            HttpUrl httpUrl = urlBuilder.build();
-            Set<String> paramKeys = httpUrl.queryParameterNames();
+        transaction.setRequestHeaders(toHttpHeaderMap(request.headers()));
+        if (hasRequestBody) {
+            if (requestBody.contentType() != null) {
+                transaction.setRequestContentType(requestBody.contentType().toString());
+            }
+            if (requestBody.contentLength() != -1) {
+                transaction.setRequestContentLength(requestBody.contentLength());
+            }
+        }
+        transaction.setRequestBodyIsPlainText(!bodyHasUnsupportedEncoding(request.headers()));
+        if (hasRequestBody && transaction.getRequestBodyIsPlainText()) {
+            BufferedSource source = getNativeSource(new Buffer(), bodyGzipped(request.headers()));
+            Buffer buffer = source.buffer();
+            requestBody.writeTo(buffer);
+            Charset charset = UTF8;
+            MediaType contentType = requestBody.contentType();
+            if (contentType != null) {
+                charset = contentType.charset(UTF8);
+            }
+            if (isPlaintext(buffer)) {
+                transaction.setRequestBody(readFromBuffer(buffer, charset));
+            } else {
+                transaction.setResponseBodyIsPlainText(false);
+            }
+        }
+        long startNs = System.nanoTime();
+        Response response;
+        try {
+            response = chain.proceed(request);
+        } catch (Exception e) {
+            transaction.setError(e.toString());
+            throw e;
+        }
+        long tookMs = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs);
 
-            if (paramKeys.contains("data")) {
-                for (String param : ApiMockHelper.paramSet) {
-                    String value = FormatHelper.parseGsonValue(param, httpUrl.queryParameter(param));
-                    if (!TextUtils.isEmpty(value)) {
-                        pathParams.append("--")
-                                .append(param)
-                                .append("--")
-                                .append(value);
-                    }
+        ResponseBody responseBody = response.body();
+
+        transaction.setRequestHeaders(toHttpHeaderMap(response.request().headers())); // includes headers added later in the chain
+        transaction.setResponseDate(new Date());
+        transaction.setTookMs(tookMs);
+        transaction.setProtocol(response.protocol().toString());
+        transaction.setResponseCode(response.code());
+        transaction.setResponseMessage(response.message());
+
+        transaction.setResponseContentLength(responseBody.contentLength());
+        if (responseBody.contentType() != null) {
+            transaction.setResponseContentType(responseBody.contentType().toString());
+        }
+        transaction.setResponseHeaders(toHttpHeaderMap(response.headers()));
+
+        transaction.setResponseBodyIsPlainText(!bodyHasUnsupportedEncoding(response.headers()));
+        if (HttpHeaders.hasBody(response) && transaction.getResponseBodyIsPlainText()) {
+            BufferedSource source = getNativeSource(response);
+            source.request(Long.MAX_VALUE);
+            Buffer buffer = source.buffer();
+            Charset charset = UTF8;
+            MediaType contentType = responseBody.contentType();
+            if (contentType != null) {
+                try {
+                    charset = contentType.charset(UTF8);
+                } catch (UnsupportedCharsetException e) {
+                    return response;
                 }
             }
+            if (isPlaintext(buffer)) {
+                strRequest = readFromBuffer(buffer.clone(), charset);
+                transaction.setResponseBody(strRequest);
+            } else {
+                transaction.setResponseBodyIsPlainText(false);
+            }
+            transaction.setResponseContentLength(buffer.size());
+        }
 
+        //TODO 利用NetSpyInterceptor原来的数据
+        StringBuilder pathParams = new StringBuilder();
+        //针对的是data参数我gson传
+        if (getDecodeRequest(strRequest).contains("data=")) {
             for (String param : ApiMockHelper.paramSet) {
-                Log.d(TAG, param + ":" + httpUrl.queryParameter(param));
-                if (paramKeys.contains(param)) {
+                String value = FormatHelper.parseGsonValue(param, getDecodeRequest(strRequest));
+                if (!TextUtils.isEmpty(value)) {
                     pathParams.append("--")
                             .append(param)
                             .append("--")
-                            .append(httpUrl.queryParameter(param));
+                            .append(value);
                 }
             }
+        }
 
-        } else {
-            if (request.body() instanceof FormBody) {
-                FormBody formBody = (FormBody) request.body();
-
-                String dataGson = "";
-                for (int i = 0; i < formBody.size(); i++) {
-                    if ("data".equals(formBody.name(i))) {
-                        dataGson = formBody.value(i);
-                        break;
-                    }
-                }
-                if (!TextUtils.isEmpty(dataGson)) {
-                    for (String param : ApiMockHelper.paramSet) {
-                        String value = FormatHelper.parseGsonValue(param, dataGson);
-                        if (!TextUtils.isEmpty(value)) {
-                            pathParams.append("--")
-                                    .append(param)
-                                    .append("--")
-                                    .append(value);
-                        }
-                    }
-                }
-
-                for (int i = 0; i < formBody.size(); i++) {
-                    Log.d(TAG, formBody.name(i) + ":" + formBody.value(i));
-                    if (ApiMockHelper.paramSet.contains(formBody.name(i))) {
-                        pathParams.append("--")
-                                .append(formBody.name(i))
-                                .append("--")
-                                .append(formBody.value(i));
-                    }
-                }
+        //针对是method等参数直接传值
+        for (String param : ApiMockHelper.paramSet) {
+            if (getDecodeRequest(strRequest).contains(param + "=")) {
+                pathParams.append("--")
+                        .append(param)
+                        .append("--")
+                        .append(FormatHelper.getParamValue(param, getDecodeRequest(strRequest)));
             }
         }
 
@@ -107,5 +168,89 @@ public final class ApiMockInterceptor implements Interceptor {
                 .build();
         Request.Builder builder = request.newBuilder();
         return chain.proceed(builder.url(newHttpUrl).build());
+
+    }
+
+    private BufferedSource getNativeSource(Response response) throws IOException {
+        if (bodyGzipped(response.headers())) {
+            BufferedSource source = response.peekBody(maxContentLength).source();
+            if (source.buffer().size() < maxContentLength) {
+                return getNativeSource(source, true);
+            } else {
+            }
+        }
+        return response.body().source();
+    }
+
+    private BufferedSource getNativeSource(BufferedSource input, boolean isGzipped) {
+        if (isGzipped) {
+            GzipSource source = new GzipSource(input);
+            return Okio.buffer(source);
+        } else {
+            return input;
+        }
+    }
+
+    private boolean bodyHasUnsupportedEncoding(Headers headers) {
+        String contentEncoding = headers.get("Content-Encoding");
+        return contentEncoding != null &&
+                !contentEncoding.equalsIgnoreCase("identity") &&
+                !contentEncoding.equalsIgnoreCase("gzip");
+    }
+
+    private boolean bodyGzipped(Headers headers) {
+        String contentEncoding = headers.get("Content-Encoding");
+        return "gzip".equalsIgnoreCase(contentEncoding);
+    }
+
+    /**
+     * 通过采样部分数据判断是否可读
+     */
+    private boolean isPlaintext(Buffer buffer) {
+        try {
+            Buffer prefix = new Buffer();
+            long byteCount = buffer.size() < 64 ? buffer.size() : 64;
+            buffer.copyTo(prefix, 0, byteCount);
+            for (int i = 0; i < 16; i++) {
+                if (prefix.exhausted()) {
+                    break;
+                }
+                int codePoint = prefix.readUtf8CodePoint();
+                if (Character.isISOControl(codePoint) && !Character.isWhitespace(codePoint)) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (EOFException e) {
+            return false; // Truncated UTF-8 sequence.
+        }
+    }
+
+    private String readFromBuffer(Buffer buffer, Charset charset) {
+        long bufferSize = buffer.size();
+        long maxBytes = Math.min(bufferSize, maxContentLength);
+        String body = "";
+        try {
+            body = buffer.readString(maxBytes, charset);
+        } catch (EOFException e) {
+        }
+        return body;
+    }
+
+    public String getDecodeRequest(String requestBody) {
+        try {
+            return URLDecoder.decode(requestBody, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return requestBody;
+        }
+    }
+
+
+    private Map<String, String> toHttpHeaderMap(Headers headers) {
+        Map<String, String> headerMap = new HashMap<>();
+        for (int i = 0, count = headers.size(); i < count; i++) {
+            headerMap.put(headers.name(i), headers.value(i));
+        }
+        return headerMap;
     }
 }
