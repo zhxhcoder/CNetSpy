02d8a4a2e693f317b3b3ea31979b77d32efe6ada
zhxh <zhxhcoder@gmail.com>
2021-02-04 11:05:46 +0800

    postTrackRecords

--- app/src/main/java/com/creditease/netspy/demo/MainActivity.java
+++ app/src/main/java/com/creditease/netspy/demo/MainActivity.java
@@ -12,6 +12,7 @@ import com.creditease.netspy.ApiMockHelper;
 import com.creditease.netspy.demo.netspy.BugActivity;
 import com.creditease.netspy.demo.netspy.NetActivity;
 import com.creditease.netspy.demo.socket.SocketMainActivity;
+import com.creditease.netspy.inner.support.OkHttpHelper;
 
 /**
  * Created by zhxh on 2019/06/20
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button netButton;
     Button bugButton;
+    Button trackButton;
 
     private ShakeDetector shakeDetector = null;
     private SensorManager sensorManager;
@@ -32,9 +34,38 @@ public class MainActivity extends AppCompatActivity {
 
         netButton = findViewById(R.id.netButton);
         bugButton = findViewById(R.id.bugButton);
+        trackButton = findViewById(R.id.trackButton);
         findViewById(R.id.netButton).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, NetActivity.class)));
         findViewById(R.id.bugButton).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, BugActivity.class)));
         findViewById(R.id.socketButton).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SocketMainActivity.class)));
+
+        trackButton.setOnClickListener(v -> {
+            OkHttpHelper.getInstance().postTrackRecords("", "", "action11", "report11", null);
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            OkHttpHelper.getInstance().postTrackRecords("", "", "action22", "report22", null);
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            OkHttpHelper.getInstance().postTrackRecords("", "", "action33", "report33", null);
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            OkHttpHelper.getInstance().postTrackRecords("", "", "action44", "report44", null);
+        });
     }
 
     @Override
