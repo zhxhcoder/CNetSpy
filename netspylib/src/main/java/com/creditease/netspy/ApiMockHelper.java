5b1b80f53eb97fd510b7af2e2976bae0c767e7b3
zhxh <zhxhcoder@gmail.com>
2021-03-29 09:44:56 +0800

    getParamSet

--- netspylib/src/main/java/com/creditease/netspy/ApiMockHelper.java
+++ netspylib/src/main/java/com/creditease/netspy/ApiMockHelper.java
@@ -43,7 +43,7 @@ public final class ApiMockHelper {
             return;
         }
         paramSet = new HashSet<String>();
-        for (String s : strParamSet.split(",")) {
+        for (String s : strParamSet.split("\\s*,\\s*")) {
             addParams(s);
         }
     }
